ff3f79eca4d232456ffc386ae3362400900bec2b
DESKTOP-KT09742\seoil <didska0524@gmail.com>
2023-03-24 21:09:32 +0900

    20230324 commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,22 +15,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig { // AppConfig 처럼 객체를 생성하고 관리하면서 의존관계를 연결해주는것을 IoC 컨테이너 또는 DI 컨테이너 라 한다.
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderservice -> new MemoryMemberRepository()
+    // 결과적으로 각각 다른 2개의 MemoryMemberRepository 가 생성되면서 싱글톤이 깨지는 것 처럼 보인다.
+    // 스프링 컨테이너는 이 문제를 어떻게 해결할까?
+
+
     @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
-    public DiscountPolicy discountPolicy() {return new RateDiscountPolicy();
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
