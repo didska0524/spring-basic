e446e745b35ecbcf4d9b522ade16dc47ab7d4132
DESKTOP-KT09742\seoil <didska0524@gmail.com>
2023-04-09 22:55:45 +0900

    lombok commit

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //롬복을 이용하면 final 이 붙은 필드를 모아서 생성자를 자동으로 만들어준다!!
 public class OrderServiceImpl implements  OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
@@ -21,10 +23,10 @@ public class OrderServiceImpl implements  OrderService{
         this.discountPolicy = discountPolicy;
     }*/
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    /*public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
 
 
